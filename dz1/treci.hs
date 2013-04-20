20266fe0809606f9f7c522c7162623a3daf556a0
dtrupcevic <dominik.trupcevic@gmail.com>
2013-04-20 20:43:14 +0200

    treci zad

--- dz1/treci.hs
+++ dz1/treci.hs
@@ -4,5 +4,9 @@ Pozitivan broj neka rotira ulijevo za zadani broj mjesta, a
 negativan udesno.
 -}
 rotl :: Int -> [a] -> [a]
-rotl n xs | n > 0 = 
-          |
+rotl n xs | length xs == 0 = xs
+          | n > 0          = drop (n `mod` length xs) xs 
+                             ++ take (n `mod` length xs) xs
+          | n < 0          = drop (length xs - abs n `mod` length xs) xs
+                             ++ take (length xs - abs n `mod` length xs) xs
+          | otherwise      = xs
